9951fb66174e318f446d919030345800f9bf8309
gurasog <cognigraph@yandex.ru>
2021-01-11 17:51:39 +0300

    simple updates in main code

--- dash_for_editor_with_spike_data_on_classical_rats.py
+++ dash_for_editor_with_spike_data_on_classical_rats.py
@@ -28,13 +28,14 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output,State
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 styles = {
     'pre': {
         'border': 'thin lightgrey solid',
@@ -743,12 +744,13 @@ def controle_spike_number(n_clicks_ok, n_clicks_previous_spike, n_clicks_next_sp
 
 
 if __name__ == '__main__':
-    #app.run_server(host='127.0.0.1', port=8100, debug=True,use_reloader=False, dev_tools_hot_reload=False)
-    app.run_server(debug=True, use_reloader=False, dev_tools_hot_reload=False)
+    app.run_server(host='127.0.0.1', port=8100, debug=True,use_reloader=False, dev_tools_hot_reload=False)
+    #app.run_server(host='https://neuroimaging.group/1',debug=True, use_reloader=False, dev_tools_hot_reload=False)
     
     
     
-    
+    #8100
+    #8101
     
     
     
