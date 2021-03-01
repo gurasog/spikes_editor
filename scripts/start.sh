2864b690d31bc1137a924cff3d2563455e1b84c9
andreasxp <andres.zhukov@gmail.com>
2021-03-01 22:45:24 +0300

    Update start.sh with new data

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,8 +10,11 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 # Launch scripts
-python3.9 dash_for_editor_with_spike_data_on_classical_rats.py  > /dev/null 2>&1 &
-python3.9 dash_for_editor_with_spike_data.py                    > /dev/null 2>&1 &
+python3.9 build_dash.py Rat_spikes_series_dataset.json Rat_spikes_series.mat                           10125 > /dev/null 2>&1 &
+python3.9 build_dash.py Rat_spikes_series_concervative_dataset.json Rat_spikes_series_concervative.mat 10126 > /dev/null 2>&1 &
+python3.9 build_dash.py Rat_spikes_28_long_alternative_dataset.json Rat_spikes_28_long_alternative.mat 10127 > /dev/null 2>&1 &
+python3.9 build_dash.py Rat_spikes_28_dataset.json Rat_spikes_28.mat                                   10128 > /dev/null 2>&1 &
+python3.9 build_dash.py Rat_spikes_28_long_dataset.json Rat_spikes_28_long.mat                         10129 > /dev/null 2>&1 &
 
 disown -a  # Detach all jobs
 deactivate  # Deactivate python environment
